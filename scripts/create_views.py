9376d29c53a3d47c32609ce51d4b06c536aa999d
Mrityunjay Pathak <themrityunjaypathak@gmail.com>
2025-11-04 08:51:35 +0530

    feat: add initial library imports in create_views script

---
+++ scripts/create_views.py
@@ -0,0 +1,7 @@
+# ---------------- Loading Python Packages ---------------- 
+
+# Importing Libraries
+import os
+import logging
+from dotenv import load_dotenv
+from sqlalchemy import create_engine, text
